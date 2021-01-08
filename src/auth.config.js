221c07b4798658fe8e792e470f8bcbd4762f2667
Samira Mc Queen <samira.mcqueen@gmail.com>
2021-01-07 20:11:31 -0400

    Update App and Test Log in

---
+++ src/auth.config.js
@@ -0,0 +1,4 @@
+export const authConfig = {
+    domain: 'dev-hy9hjuex.us.auth0.com',
+    clientId: 'UFydB8WbKI2xkzxf4PgAXdFd9Ogd8v4d'
+  }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from './react-auth0-spa';
+import { authConfig } from './auth.config';
 import reportWebVitals from './reportWebVitals';
 
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={authConfig.domain}
+    client_id={authConfig.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
