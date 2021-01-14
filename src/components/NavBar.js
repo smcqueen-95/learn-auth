eae509ccf44298c117d0102dbe5d16c7fd049fdb
Samira Mc Queen <samira.mcqueen11@gmail.com>
2021-01-14 11:47:16 -0400

    Tutorial Wrap Up

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { useAuth0 } from '../react-auth0-spa';
+import './Navbar.css'
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
   return (
     <div>
-      {!isAuthenticated && (
-        <button onClick={() => loginWithRedirect({})}>Log in</button>
-      )}
+    {!isAuthenticated && (
+      <button onClick={() => loginWithRedirect({})}>Log in</button>
+    )}
 
-      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
-    </div>
+    {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+
+    {isAuthenticated && (
+      <span>
+        <Link to="/">Home </Link>
+        <Link to="/profile">Profile</Link>
+      </span>
+    )}
+  </div>
   );
 };
 
